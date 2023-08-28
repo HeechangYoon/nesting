1069a745c6efc9330b779fc1b5afa21d102ff84a
User <yhc2222@gmail.com>
2023-08-28 10:53:46 +0900

    aa

--- agent/network.py
+++ agent/network.py
@@ -21,9 +21,18 @@ class Network(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
         # self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3)
-        self.fc1 = nn.Linear(32 * 52 * 11, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 256)
+        self.fc1_x = nn.Linear(32 * 52 * 11, 1024)
+        self.fc2_x = nn.Linear(1024, 512)
+        self.fc3_x = nn.Linear(512, 256)
+        self.fc1_y = nn.Linear(32 * 52 * 11, 1024)
+        self.fc2_y = nn.Linear(1024, 512)
+        self.fc3_y = nn.Linear(512, 256)
+        self.fc1_a = nn.Linear(32 * 52 * 11, 1024)
+        self.fc2_a = nn.Linear(1024, 512)
+        self.fc3_a = nn.Linear(512, 256)
+        self.fc1_v = nn.Linear(32 * 52 * 11, 1024)
+        self.fc2_v = nn.Linear(1024, 512)
+        self.fc3_v = nn.Linear(512, 256)
         self.fc_x_pi = nn.Linear(256, x_action_size)
         self.fc_y_pi = nn.Linear(256, y_action_size)
         self.fc_a_pi = nn.Linear(256, a_action_size)
@@ -36,9 +45,9 @@ class Network(nn.Module):
         x = self.pool2(x)
 
         x = x.contiguous().view(-1, 32 * 52 * 11)
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.leaky_relu(self.fc1_x(x))
+        x = F.leaky_relu(self.fc2_x(x))
+        x = F.leaky_relu(self.fc3_x(x))
         x = self.fc_x_pi(x)
         return x
 
@@ -49,9 +58,9 @@ class Network(nn.Module):
         x = self.pool2(x)
 
         x = x.contiguous().view(-1, 32 * 52 * 11)
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.leaky_relu(self.fc1_y(x))
+        x = F.leaky_relu(self.fc2_y(x))
+        x = F.leaky_relu(self.fc3_y(x))
         x = self.fc_y_pi(x)
         return x
 
@@ -62,9 +71,9 @@ class Network(nn.Module):
         x = self.pool2(x)
 
         x = x.contiguous().view(-1, 32 * 52 * 11)
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.leaky_relu(self.fc1_a(x))
+        x = F.leaky_relu(self.fc2_a(x))
+        x = F.leaky_relu(self.fc3_a(x))
         x = self.fc_a_pi(x)
         return x
 
@@ -75,9 +84,9 @@ class Network(nn.Module):
         x = self.pool2(x)
 
         x = x.contiguous().view(-1, 32 * 52 * 11)
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.leaky_relu(self.fc1_v(x))
+        x = F.leaky_relu(self.fc2_v(x))
+        x = F.leaky_relu(self.fc3_v(x))
         v = self.fc_v(x)
         return v
 
